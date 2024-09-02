506e7b66a6c75fe94ccd0b2b2b2b43f58db32a4d
Valdeilson Lima <valdeilsonbdl56@gmail.com>
2024-09-02 20:27:56 -0300

    Inserido atrubuto rua

--- src/br/com/clinica/mode1/Paciente.java
+++ src/br/com/clinica/mode1/Paciente.java
@@ -1,5 +1,35 @@
 package br.com.clinica.mode1;
 
 public class Paciente {
+	 int idPaciente;
+	 String nome;
+	 String cpf;
+	 String dataNascimento;
+	 String sexo;
+	 String endereco;
+	 int numero;
+	 String bairro;
+	 String cidade;
+	 String uf;
+	 String telefone;
+	 String email;
+	 String Rua;
+	
+	
+	public Paciente(int idPaciente, String nome, String cpf, String dataNascimento, String sexo, String endereco, int numero, String bairro, String cidade, String uf, String telefone, String email, String Rua) {
+        this.idPaciente = idPaciente;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.dataNascimento = dataNascimento;
+        this.sexo = sexo;
+        this.endereco = endereco;
+        this.numero = numero;
+        this.bairro = bairro;
+        this.cidade = cidade;
+        this.uf = uf;
+        this.telefone = telefone;
+        this.email = email;
+        this.Rua = Rua;
+    }
 
 }
